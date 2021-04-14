513b91e7159118989a19b55768c35dbeb23f92d4
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-14 19:04:22 -0400

    insert plugin for webpack bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
         $: "jquery",
         jQuery: "jquery"
       }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      })
     ],
     mode: 'development'
 };
