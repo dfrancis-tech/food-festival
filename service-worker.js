7ade350e65cc39a0988eefe051f7482a62e25fcc
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-04-17 19:59:33 -0400

    add files to cache in service worker js file

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// define which files we'd like to cache
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+];
+
+// assign global consonants
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+})
